5500c14d909da2c95a27052740bf69d28ea814b6
01Alekje <01Alexanderkjellberg@gmail.com>
2024-05-31 16:29:02 +0200

    first commit (not really lol)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<div class="main-container">
+    <h3>Welcome to the temporary landing-page of RonkNet Server!</h3>
+    <p>You are being served by RonkNet Server, located in my apartments 'server-room'. What is the purpose of this website? I don't know. We'll see in the future. </p>
+    <footer>
+        <h3>Contact / Links</h3>
+        <a href="mailto:01Alexanderkjellberg@gmail.com">01Alexanderkjellberg@gmail.com</a>
+        <a href="https://www.linkedin.com/in/alexander-kjellberg-157259110/">LinkedIn</a>
+    </footer>
+</div>
+
+<style>
+    :global(body) {
+        margin: 0;
+        font-family: "Open Sans", sans-serif;
+        background-color: rgb(44, 44, 44);
+        text-align: center;
+    }
+    .main-container {
+        background-color: rgb(196, 196, 196);
+        box-shadow: 0 0 5px 5px purple;
+        width: 750px;
+        height: 80dvh;
+        margin: auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    footer {
+        display: flex;
+        flex-direction: column;
+    }
+    a {
+        color: black;
+    }
+</style>
